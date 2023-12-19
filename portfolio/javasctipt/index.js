df7b76d5b8717a61d9cc5abd0dc1e3d0852b59ab
mani <manimanideep1234@gmail.com>
2023-12-19 06:46:47 +0530

    second commit

--- portfolio/javasctipt/index.js
+++ portfolio/javasctipt/index.js
@@ -6,4 +6,26 @@ $(document).ready(function() {
   
   });
 
+  const glitchyText = document.querySelector(".glitchy-text");
+  const glitchImage = document.querySelector(".glitchy-text img");
   
+  const colors = ["#FF4655", // Coral
+  "#A300D9", // Purple
+  "#00FF9F"];
+  let currentColor = 0;
+  
+  setInterval(() => {
+    glitchyText.style.color = colors[currentColor];
+    glitchImage.style.filter = `hue-rotate(${currentColor * 30}deg)`;
+    currentColor = (currentColor + 1) % colors.length;
+  }, 1000);
+  
+  setInterval(() => {
+    glitchyText.style.animationDuration = `${Math.random() * 2 + 0.5}s`;
+    glitchImage.style.animationDuration = `${Math.random() * 2 + 0.5}s`;
+  }, 3000);
+  
+
+
+
+
